67e0b8b7bc3c3526b77e9a874e0f4c2fb0ff1e1b
rferrari <rferrariwd@gmail.com@>
2025-06-09 20:50:25 -0300

    disable: middleware

--- src/app/api/v1/auth.ts
+++ src/app/api/v1/auth.ts
@@ -4,12 +4,15 @@ import type { NextRequest } from 'next/server';
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 export async function authenticateToken(request: NextRequest): Promise<boolean> {
+  return true;
+
+  
   // Get the host from the headers
   // const host = request.headers.get('x-forwarded-host') || request.nextUrl.host;
   
   // Check if the host is 'localhost:3000' and skip authentication
   // if (host === 'localhost:3000') {
-    return true;
+  // return true;
   // }
 
   
