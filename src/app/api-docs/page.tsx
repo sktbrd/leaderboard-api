93c78c3851e1218b31b850392c190e5726fd6b39
xvlad <116202536+sktbrd@users.noreply.github.com>
2025-08-06 15:21:04 -0300

    style: enhance swagger dark theme contrast

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -2,6 +2,7 @@
 
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import './swagger-theme.css';
 import { swaggerDocument } from '@/swagger';
 
 export default function ApiDocs() {
